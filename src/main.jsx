32aadba9d34b525bfa415df8776e660611283ece
Alvaro Borrero <ajhades@hotmail.com>
2024-10-27 15:34:33 -0500

    unit test CounterApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelloWorldApp } from "./HelloWorldApp";
-import './styles.css';
+import "./styles.css";
 import { CounterApp } from "./CounterApp";
 
-ReactDOM.createRoot( document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <CounterApp value={12}/> */}
-    <HelloWorldApp title="Hello" />
+    <CounterApp value={100} />
+    {/* <HelloWorldApp title="Hello" /> */}
   </React.StrictMode>
 );
-
